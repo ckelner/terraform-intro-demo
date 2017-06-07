f244e06eeb09d2e4791c42b46d0f27dfba7887d9
ckelner <chris.kelner@gmail.com>
2017-06-07 11:01:39 -0400

    add outputs

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+#############################################################################
+# Outputs
+# https://www.terraform.io/docs/configuration/outputs.html
+#############################################################################
+output "vpc_id" {
+    value = "${aws_vpc.main.id}"
+}
+output "subnet_ids" {
+    value = ["${aws_subnet.public.*.id}"]
+}
+output "elb_id" {
+    value = "${aws_elb.elb.id}"
+}
+output "elb_instances" {
+    value = "${aws_elb.elb.instances}"
+}
+output "asg_id" {
+    value = "${aws_autoscaling_group.web_asg.id}"
+}
