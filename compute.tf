0e4bcd0062115d0b69435b54bac3d4e0a78a0283
ckelner <chris.kelner@gmail.com>
2017-06-07 08:10:24 -0400

    Finish compute (asg) resources

--- compute.tf
+++ compute.tf
@@ -6,30 +6,38 @@ resource "aws_key_pair" "aws_ssh_key" {
   key_name   = "${var.common_name}-${terraform.env}"
   public_key = "${var.aws_public_key_material}"
 }
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/*resource "aws_autoscaling_group" "web-asg" {
-  availability_zones   = ["${split(",", var.availability_zones)}"]
-  name                 = "terraform-example-asg"
-  max_size             = "${var.asg_max}"
-  min_size             = "${var.asg_min}"
-  desired_capacity     = "${var.asg_desired}"
-  force_delete         = true
-  launch_configuration = "${aws_launch_configuration.web-lc.name}"
-  load_balancers       = ["${aws_elb.web-elb.name}"]
-
-  #vpc_zone_identifier = ["${split(",", var.availability_zones)}"]
-  tag {
-    key                 = "Name"
-    value               = "web-asg"
-    propagate_at_launch = "true"
+#
+# AWS Autoscaling Group
+# https://www.terraform.io/docs/providers/aws/r/autoscaling_group.html
+#
+resource "aws_autoscaling_group" "web_asg" {
+  name                      = "${var.common_name}-${terraform.env}"
+  vpc_zone_identifier       = ["${aws_subnet.public.*.id}"]
+  max_size                  = "${var.aws_asg_max}"
+  min_size                  = "${var.aws_asg_min}"
+  desired_capacity          = "${var.aws_asg_desired}"
+  force_delete              = true
+  launch_configuration      = "${aws_launch_configuration.web_lc.name}"
+  load_balancers            = ["${aws_elb.elb.name}"]
+  termination_policies      = ["OldestLaunchConfiguration"]
+  wait_for_capacity_timeout = "10m" # 5m isn't quite enough time
+  min_elb_capacity          = "${var.aws_asg_min}"
+  tag = {
+    key                     = "Name"
+    value                   = "${var.common_name}-${terraform.env}"
+    propagate_at_launch     = "true"
   }
 }
-
-resource "aws_launch_configuration" "web-lc" {
-  name          = "terraform-example-lc"
-  image_id      = "${lookup(var.aws_amis, var.aws_region)}"
-  instance_type = "${var.instance_type}"
-  security_groups = ["${aws_security_group.default.id}"]
-  user_data       = "${file("userdata.sh")}"
-  key_name        = "${var.key_name}"
-}*/
+#
+# AWS Launch Config
+# https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+#
+resource "aws_launch_configuration" "web_lc" {
+  name_prefix                 = "${var.common_name}-${terraform.env}"
+  image_id                    = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type               = "${var.aws_instance_type}"
+  security_groups             = ["${aws_security_group.web_sg.id}"]
+  user_data                   = "${file("userdata.sh")}"
+  key_name                    = "${aws_key_pair.aws_ssh_key.key_name}"
+  associate_public_ip_address = true
+}
