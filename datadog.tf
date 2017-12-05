f1c74951aa150bd0aeb15744bab451e27debfd6a
Chris Kelner <ckelner@users.noreply.github.com>
2017-12-05 10:36:20 -0500

    Change from hipchat to slack

--- datadog.tf
+++ datadog.tf
@@ -13,7 +13,7 @@ provider "datadog" {
 resource "datadog_monitor" "foo" {
   name               = "Name for monitor foo"
   type               = "metric alert"
-  message            = "Monitor triggered. Notify: @hipchat-channel"
+  message            = "Monitor triggered. Notify: @slack"
   escalation_message = "Escalation message @pagerduty"
   query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 2"
   thresholds {
